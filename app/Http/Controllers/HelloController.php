5f9e8b485e659fc3cb639025bb13aafa2512ca62
takusomikke <takusomikke@gmail.com>
2020-07-25 21:00:42 +0900

    Use sometimes method (p.151)

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -30,15 +30,24 @@ class HelloController extends Controller
         $rules = [
                 'name' => 'required',
                 'mail' => 'email',
-                'age' => 'numeric|between:0,150',
+                'age' => 'numeric',
         ];
         $messages = [
             'name.required' => '名前は必ず入力してください。',
             'mail.email' => 'メールアドレスが必要です。',
             'age.numeric' => '年齢を整数で記入ください。',
-            'age.between' => '年齢は0～150の間で入力ください。',
+            'age.min' => '年齢はゼロ歳以上で記入ください。',
+            'age.max' => '年齢は200歳以下で入力ください。',
         ];
         $validator = Validator::make($request->all(), $rules, $messages );
+
+        $validator->sometimes('age', 'min:0', function($input){
+            return !is_int($input->age);
+        });
+
+        $validator->sometimes('age', 'max:200', function($input){
+            return !is_int($input->age);
+        });
 
         if ($validator->fails()) {
             return redirect('/hello')
