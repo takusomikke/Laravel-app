d83e05ce7277ebeae3531fcd6026af2af4aead83
takusomikke <takusomikke@gmail.com>
2020-07-25 15:44:41 +0900

    Use $loop variable

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,17 +9,17 @@
 </head>
 <body>
     <h1>Blade/Index</h1>
-    <p>&#064;forディレクティブの例</p>
+    <p>&#064;foreachディレクティブの例</p>
     <ol>
-        @for ($i = 1;$i < 100;$i++)
-            @if ($i % 2 == 1)
-                @continue
-            @elseif ($i <= 10)
-            <li>No, {{$i}}
-            @else
-                @break
+        @foreach ($data as $item)
+            @if ($loop->first)
+                <p>※データ一覧</p><ul>
             @endif
-        @endfor
+            <li>No,{{$loop->iteration}}. {{$item}}</li>
+            @if ($loop->last)
+                </ul><p>--ここまで</p>
+            @endif
+        @endforeach
     </ol>
 </body>
 </html>
