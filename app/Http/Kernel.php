b20037d75c555164e666b871bc2696fa82db7bf9
takusomikke <takusomikke@gmail.com>
2020-07-25 18:39:51 +0900

    Register in middleware group

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\HelloMiddleware::class,
     ];
 
     /**
@@ -43,6 +42,9 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+        'hello' => [
+            \App\Http\Middleware\HelloMiddleware::class,
         ],
     ];
 
