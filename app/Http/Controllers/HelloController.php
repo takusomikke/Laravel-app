0a5c1adbef4b6c415c4f87e5982d3bb402cf240e
takusomikke <takusomikke@gmail.com>
2020-07-25 21:24:23 +0900

    Create original validator

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -25,36 +25,8 @@ class HelloController extends Controller
         return view('hello.index', ['msg'=>$msg, ]);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $rules = [
-                'name' => 'required',
-                'mail' => 'email',
-                'age' => 'numeric',
-        ];
-        $messages = [
-            'name.required' => '名前は必ず入力してください。',
-            'mail.email' => 'メールアドレスが必要です。',
-            'age.numeric' => '年齢を整数で記入ください。',
-            'age.min' => '年齢はゼロ歳以上で記入ください。',
-            'age.max' => '年齢は200歳以下で入力ください。',
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages );
-
-        $validator->sometimes('age', 'min:0', function($input){
-            return !is_int($input->age);
-        });
-
-        $validator->sometimes('age', 'max:200', function($input){
-            return !is_int($input->age);
-        });
-
-        if ($validator->fails()) {
-            return redirect('/hello')
-                        ->withErrors($validator)
-                        ->withInput();
-        }
-
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
