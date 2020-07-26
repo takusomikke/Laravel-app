d62344da8b1d5b25b6596ece84caa9c785f66df9
takusomikke <takusomikke@gmail.com>
2020-07-27 01:50:50 +0900

    Enable to add new data into person model

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,9 @@ Route::get('hello/del','HelloController@del');
 Route::post('hello/del','HelloController@remove');
 Route::get('hello/show','HelloController@show');
 
-Route::get('hello/person','PersonController@index');
-
+Route::get('person','PersonController@index');
 Route::get('person/find','PersonController@find');
 Route::post('person/find','PersonController@search');
+Route::get('person/add','PersonController@add');
+Route::post('person/add','PersonController@create');
 
