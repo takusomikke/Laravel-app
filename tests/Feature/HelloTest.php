6e295a9d13ef6ae54143f14d50cdcd696f5d9e07
takusomikke <takusomikke@gmail.com>
2020-08-02 02:13:09 +0900

    Add unit(response) test

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\User;
 
 class HelloTest extends TestCase
 {
@@ -12,13 +13,17 @@ class HelloTest extends TestCase
     {
         $this->assertTrue(true);
 
-        $arr = [];
-        $this->assertEmpty($arr);
+        $response = $this->get('/');
+        $response->assertStatus(200);
 
-        $msg = "Hello";
-        $this->assertEquals('Hello', $msg);
+        $response = $this->get('/hello');
+        $response->assertStatus(302);
 
-        $n = random_int(0, 100);
-        $this->assertLessThan(100, $n);
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('/hello');
+        $response->assertStatus(200);
+
+        $response = $this->get('/no_route');
+        $response->assertStatus(404);
     }
 }
