ec3a2dce175126a723ddb600f4ab2da9940201d6
takusomikke <takusomikke@gmail.com>
2020-08-01 10:59:10 +0900

    Add a page for editing person

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -39,9 +39,27 @@ class PersonController extends Controller
         $this->validate($request, Person::$rules);
         $person = new Person;
         $form = $request->all();
-        // unset($form['_token']);
         unset($form['send']);
         $person->fill($form)->save();
         return redirect('/person');
     }
+
+    public function edit(Request $request)
+    {
+        $person = Person::find($request->id);
+        return view('person.edit', ['form' => $person]);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, Person::$rules);
+        $person = Person::find($request->id);
+        echo $person;
+        $form = $request->all();
+        unset($form['send']);
+        $person->fill($form)->save();
+        return redirect('/person');
+    }
+
+
 }
