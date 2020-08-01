c67968207b405a362ab38e6cfe81f9d5049dac88
takusomikke <takusomikke@gmail.com>
2020-08-02 02:29:16 +0900

    Add unit(database) test

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -6,24 +6,39 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\User;
+use App\Person;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class HelloTest extends TestCase
 {
+    use DatabaseMigrations;
+
     public function testHello()
     {
-        $this->assertTrue(true);
+        factory(User::class)->create([
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
+        factory(User::class, 10)->create();
 
-        $response = $this->get('/');
-        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', [
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
+        
+        factory(Person::class)->create([
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
+        factory(Person::class, 10)->create();
 
-        $response = $this->get('/hello');
-        $response->assertStatus(302);
-
-        $user = factory(User::class)->create();
-        $response = $this->actingAs($user)->get('/hello');
-        $response->assertStatus(200);
-
-        $response = $this->get('/no_route');
-        $response->assertStatus(404);
+        $this->assertDatabaseHas('people', [
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
     }
 }
