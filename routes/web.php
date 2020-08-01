67bdb231a6c49ad144ce03dbd6b906c73327fc59
takusomikke <takusomikke@gmail.com>
2020-08-01 13:27:06 +0900

    Add Board

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::post('person/edit','PersonController@update');
 Route::get('person/del','PersonController@delete');
 Route::post('person/del','PersonController@remove');
 
+Route::get('board', 'BoardController@index');
+Route::get('board/add', 'BoardController@add');
+Route::post('board/add', 'BoardController@create');
